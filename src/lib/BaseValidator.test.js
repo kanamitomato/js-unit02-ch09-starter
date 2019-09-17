d8a533a4840bcfa3e54ed0ba25cac436baa14312
Kanami <kanami.h217@gmail.com>
2019-09-17 13:57:42 +0900

    modify BaseValidator

--- src/lib/BaseValidator.test.js
+++ src/lib/BaseValidator.test.js
@@ -1,22 +1,12 @@
 import BaseValidator from './BaseValidator';
 
 describe('BaseValidator', () => {
-    it("should pass with valid data", async () => {
+    it("should pass with valid data", () => {
         const email = "/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i";
         const validator = new BaseValidator(email);
         return validator._cannotEmpty()
             .then((res) => {
-                expect(res).toMatch(validator)
+                expect(res).toBe(validator)
             })
-    });
-
-    it("should return error with empty value", () => {
-        const value = "";
-        const validator = BaseValidator(value);
-        return validator.validate()
-            .then((res) => {
-                expect(res.success).toBeFalsy();
-                expect(res.message).toBe('パスワードは必須です。')
-            });
     });
 })
